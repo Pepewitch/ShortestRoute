ee2bc5c90c975edb04a180d53e6b78f39a2da7ed
Witchayut Jongpattanasombut <skpepe134@gmail.com>
2017-12-06 19:16:42 +0700

    edit Header

--- ShortestRoute/src/view/HeaderBar.java
+++ ShortestRoute/src/view/HeaderBar.java
@@ -7,25 +7,29 @@ import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import model.Player;
 
 public class HeaderBar extends Canvas {
-	private StatusView status;
+	private Player player;
 	private Image image;
-	public HeaderBar(double width, double height, StatusView st) {
+	private GraphicsContext gc;
+	private double scaleWidHead;
+	private double scaleHeiHead;
+	public HeaderBar(double width, double height, Player player) {
 		super(width, height/12);
-		this.status = st;
-		GraphicsContext gc = this.getGraphicsContext2D();
+		this.player = player;
+		gc = this.getGraphicsContext2D();
 		Font theFont = Font.font("Times New Roman", FontWeight.LIGHT, width*height/30000);
 		gc.setFont(theFont);
 		image = new Image("file:pic/"+"cube_trans.png");
 		//////
-		double scaleWidHead = this.getWidth()/5;
-		double scaleHeiHead = this.getHeight();
+		scaleWidHead = this.getWidth()/5;
+		scaleHeiHead = this.getHeight();
 		//-->Life Point
 		gc.setFill(Color.LIGHTGOLDENRODYELLOW);
 		gc.fillRect(0, 0, scaleWidHead, scaleHeiHead);
 		gc.setFill(Color.RED);
-		gc.fillText("❤ "+this.status.getLifeText(), 10, scaleHeiHead*4/5);
+		gc.fillText("❤  Life : "+this.player.life, 10, scaleHeiHead*4/5);
 		//-->Point
 		gc.setFill(Color.PALEGOLDENROD);
 		gc.fillRect(scaleWidHead, 0, scaleWidHead*2, scaleHeiHead);
@@ -54,10 +58,10 @@ public class HeaderBar extends Canvas {
 						gc.setFill(Color.LIGHTGOLDENRODYELLOW);
 						gc.fillRect(0, 0, scaleWidHead, scaleHeiHead);
 						gc.setFill(Color.RED);
-						gc.fillText("❤ "+status.getLifeText(), 10, scaleHeiHead*4/5);
+						gc.fillText("❤  Life : "+player.life, 10, scaleHeiHead*4/5);
 					}
 				};
-				while(status.getPlayer().life>=0) {
+				while(player.life>=0) {
 					Platform.runLater(update);
 					try {
 						Thread.sleep(30);
@@ -68,5 +72,35 @@ public class HeaderBar extends Canvas {
 				}
 			}
 		}.start();
+	}
+	
+	public void update() {
+		scaleWidHead = this.getWidth()/5;
+		scaleHeiHead = this.getHeight();
+		//-->Life Point
+		gc.setFill(Color.LIGHTGOLDENRODYELLOW);
+		gc.fillRect(0, 0, scaleWidHead, scaleHeiHead);
+		gc.setFill(Color.RED);
+		gc.fillText("❤  Life : "+this.player.life, 10, scaleHeiHead*4/5);
+		//-->Point
+		gc.setFill(Color.PALEGOLDENROD);
+		gc.fillRect(scaleWidHead, 0, scaleWidHead*2, scaleHeiHead);
+		gc.setFill(Color.BLACK);
+		gc.fillText("★ Point", scaleWidHead+10, scaleHeiHead*4/5);
+		//-->Empty
+		gc.setFill(Color.GOLD);
+		gc.fillRect(scaleWidHead*2, 0, scaleWidHead*3, scaleHeiHead);
+		//-->Stage
+		gc.setFill(Color.GOLDENROD);
+		gc.fillRect(scaleWidHead*3, 0, scaleWidHead*4, scaleHeiHead);
+		gc.setFill(Color.BLACK);
+		gc.fillText("Stage", scaleWidHead*3+10, scaleHeiHead*4/5);
+		//-->Menu
+		gc.setFill(Color.DARKGOLDENROD);
+		gc.fillRect(scaleWidHead*4, 0, scaleWidHead, scaleHeiHead);
+		gc.setFill(Color.BLACK);
+		gc.fillText("Menu", scaleWidHead*4+10, scaleHeiHead*4/5);
+		
+		gc.drawImage(image,0, 0, getWidth(), getHeight());
 	}
 }
--- ShortestRoute/src/view/NormalGame.java
+++ ShortestRoute/src/view/NormalGame.java
@@ -12,39 +12,33 @@ import model.StartNode;
 
 public class NormalGame extends VBox{
 	public NormalGame(Stage primaryStage) {
-		super(10);
+		super(0);
 		Game game = new Game(new Map(6,6) , new Player(100 , 0 , 0 ));
 		game.getMap().setNode(4, 4, new StartNode(4,4));
 		game.getMap().setNode(2, 2, new EndNode(2,2));
 		game.setStart();
 		MapView map = new MapView(game);
 		map.update();
-		StatusView status = new StatusView(game.getPlayer());
 		BorderPane frame = new BorderPane(map);
-		
-		HeaderBar topBar = new HeaderBar(primaryStage.getWidth(), primaryStage.getHeight(), status);
-		this.getChildren().add(topBar);
-		
-//		Rectangle clip = new Rectangle(primaryStage.getWidth(),primaryStage.getHeight()-status.getHeight());
-//		frame.setClip(topBar);
+		HeaderBar topBar = new HeaderBar(primaryStage.getWidth(), primaryStage.getHeight(), game.getPlayer());
+		Rectangle clip = new Rectangle(primaryStage.getWidth(),primaryStage.getHeight()-topBar.getHeight());
+		frame.setClip(clip);
 		
 		primaryStage.widthProperty().addListener((obs, oldVal, newVal) -> {
 		     // Do whatever you want
-			this.getChildren().clear();
-			HeaderBar newTopBar = new HeaderBar((double)newVal, primaryStage.getHeight(), status);
-			this.getChildren().add(newTopBar);
-			this.getChildren().addAll(status , frame);
-
-
+			topBar.setWidth((double)newVal);
+			topBar.update();
+			clip.setWidth((double)newVal);
+			frame.setClip(clip);
 		});
 		primaryStage.heightProperty().addListener((obs, oldVal, newVal) -> {
 		     // Do whatever you want
-			this.getChildren().clear();
-			HeaderBar newTopBar = new HeaderBar(primaryStage.getWidth(), (double)newVal, status);
-			this.getChildren().add(newTopBar);
-			this.getChildren().addAll(status , frame);
+			topBar.setHeight((double)newVal/12);
+			topBar.update();
+			clip.setHeight((double)newVal - topBar.getHeight());
+			frame.setClip(clip);
 			
 		});
-		this.getChildren().addAll(status , frame);
+		this.getChildren().addAll(topBar , frame);
 	}
 }
